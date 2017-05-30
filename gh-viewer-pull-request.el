2f254fa33d3f393c06ce86039ad838b99c935607
Yuya Minami <yuya373@me.com>
2017-05-30 18:12:31 +0900

    fix assignees, date format

--- gh-viewer-pull-request.el
+++ gh-viewer-pull-request.el
@@ -55,47 +55,62 @@
       (goto-char (point-min)))
     buf))
 
-(defmethod gh-viewer-pull-request-issues-to-string ((pr gh-issues-issue))
-  (let* ((title (format "[%s]\t%s"
+(defun gh-viewer-pull-request-format-time-string (time-string)
+  (format-time-string "%Y-%m-%d %H:%M:%S" (date-to-time time-string)))
+
+(defmethod gh-viewer-pull-request-issues-to-string ((pr gh-issues-issue) repo)
+  (let* ((title (format "#%s [%s]\t%s"
+                        (oref pr number)
                         (oref pr state)
                         (propertize (oref pr title)
                                     'face 'gh-viewer-pull-request-title-face)))
-         (info (format "#%s\topend at %s"
-                       (oref pr number)
-                       (oref pr created-at)))
+         (times (format "opened at: %s\nupdated at: %s"
+                        (gh-viewer-pull-request-format-time-string
+                         (oref pr created-at))
+                        (gh-viewer-pull-request-format-time-string
+                         (oref pr updated-at))))
          (assignees (format "assignees: %s"
                             (mapconcat #'(lambda (user)
-                                           (number-to-string
-                                            (oref user id)))
+                                           (oref user login))
                                        (oref pr assignees)
-                                       ", "))))
-    (format "%s\n%s\n%s\n" title info assignees)))
+                                       ", ")))
+         (comments (format "%s comments" (oref pr comments))))
+    (format "%s\n%s\n%s\n%s\n" title times assignees comments)))
 
 ;;;###autoload
-(defun gh-viewer-pull-request ()
+(defun gh-viewer-pull-request (&optional invalidate-cache)
   (interactive)
   (let* ((repo (gh-viewer-repo-select))
-         (api (gh-issues-api :sync nil :cache nil))
-         (issues (gh-issues-issue-list api (oref repo user) (oref repo repo)))
+         (cache (oref repo issues))
+         (issues (if (or invalidate-cache
+                         (< (length cache) 1))
+                     (gh-issues-issue-list
+                      (gh-issues-api :sync nil :cache nil)
+                      (oref repo user) (oref repo repo))
+                   cache))
          (pulls (cl-remove-if
                  #'(lambda (issue)
                      (not (gh-issues-pull-request-p (oref issue pull-request))))
                  (oref issues data)))
          (buf (gh-viewer-pull-request--create-buffer repo)))
-    (if (eq 0 (length pulls))
-        (error "No Pull Requests")
-      (with-current-buffer buf
-        (setq buffer-read-only nil)
-        (mapc #'(lambda (pr)
-                  (insert (propertize
-                           (gh-viewer-pull-request-issues-to-string pr)
-                           'url (oref (oref pr pull-request) html-url)
-                           'keymap gh-viewer-pull-request-keymap))
-                  (insert "\n"))
-              pulls)
-        (setq buffer-read-only t)
-        (goto-char (point-min)))
-      (display-buffer buf))))
+    (oset repo issues issues)
+    (gh-viewer-pull-request-render buf pulls repo)))
+
+(defun gh-viewer-pull-request-render (buf pulls repo)
+  (if (eq 0 (length pulls))
+      (error "No Pull Requests")
+    (with-current-buffer buf
+      (setq buffer-read-only nil)
+      (mapc #'(lambda (pr)
+                (insert (propertize
+                         (gh-viewer-pull-request-issues-to-string pr repo)
+                         'url (oref (oref pr pull-request) html-url)
+                         'keymap gh-viewer-pull-request-keymap))
+                (insert "\n"))
+            pulls)
+      (setq buffer-read-only t)
+      (goto-char (point-min)))
+    (display-buffer buf)))
 
 
 (provide 'gh-viewer-pull-request)
